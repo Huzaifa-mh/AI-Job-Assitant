ed3c4fdabc9536737076b5e14da475ec7de95ebc
Muhammad Jawad <m.jawad1096@gmail.com>
2026-05-17 06:30:49 +0500

    added new routes to server.js

--- Backend/server.js
+++ Backend/server.js
@@ -14,6 +14,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('./api/auth', authRoutes);
+app.use('./api/users', userRoutes);
+
 app.use(errorHandler);
 
 connectDB().then((() => {
