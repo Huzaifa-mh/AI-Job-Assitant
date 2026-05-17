7802061c569ff77ffbd197ae0de7b54434209e51
Muhammad Jawad <m.jawad1096@gmail.com>
2026-05-17 06:27:56 +0500

    server added

---
+++ Backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { connectDB } = require('./config/db');
+const { errorHandler } = require('./middleware/errorMiddleware');
+
+const authRoutes = require('.routes/authRoutes');
+const userRoutes = require ('./routes/userRoutes');
+
+dotenv.config();const app =express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(errorHandler);
+
+connectDB().then((() => {
+    app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+}));
+
