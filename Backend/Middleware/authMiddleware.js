5b2f3da686d9d7d733400a5552a8de7ca961cb4c
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2026-05-18 21:31:07 +0500

    Added Debug to fix the error in the auth Middleware

--- Backend/Middleware/authMiddleware.js
+++ Backend/Middleware/authMiddleware.js
@@ -10,8 +10,13 @@ const protect = (req, res, next) =>{
 
     const token = authHeader.split(' ')[1];
 
+    //Debuging
+    console.log('Token:', token); // Debugging log to check the token value
+    console.log(req.headers.authorization);
+    console.log(process.env.JWT_SECRET);
+
     try{
-        const decoded = jwt.verifty(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
     }catch(error){
