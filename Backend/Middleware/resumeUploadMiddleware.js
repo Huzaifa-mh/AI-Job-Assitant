c0795fe7ca5e417177aa719d5ccda4ad6e828142
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2026-05-19 17:12:49 +0500

    Added the middleware for the resume upload

---
+++ Backend/Middleware/resumeUploadMiddleware.js
@@ -0,0 +1,29 @@
+const multer = require('multer'); //for storing the file in the server
+const path = require('path');
+const { v4: uuidv4 } = require('uuid'); //for generating unique file names
+
+const storage = multer.diskStorage({
+    destination:(req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) =>{
+        const uniqueName = `${uuidv4()}${path.extname(file.originalname)}`;
+        cb(null, uniqueName);
+    },
+});
+
+const fileFilter = (req, file, cb) =>{
+    const allowed = ['.pdf', '.doc', '.docx'];
+    const ext = path.extreme(file.originalname).toLowerCase();
+    if(allowed.includes(ext)){
+        cb(null, true);
+    }else{
+        cb(new Error('Only PDF and Word Documents are allowed to be upload'), false)
+    }
+};
+
+const upload = multer ({
+    storage,fileFilter, limits: {fileSize: 5*1024*1024} //5MB Max
+});
+
+module.exports = upload;
